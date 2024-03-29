11cb7c2e3db99d4bb52853a3094acdc92e1c9ac6
Ethan Holz <ethan.holz@thoriumworks.com>
2024-03-28 19:12:53 -0500

    fix: updated examples to match

--- src/main.zig
+++ src/main.zig
@@ -7,12 +7,14 @@ const Router = fermyon_router.Router;
 const serveFile = fermyon_router.serveFile;
 
 pub fn testHandler(_: HttpRequest, response: *HttpResponse) void {
-    response.addHeader("content-type", "text/plain") catch |err| {
+    response.addHeaders(.{
+        .{ "content-type", "text/plain" },
+    }) catch |err| {
         std.debug.print("Error writing to response: {any}\n", .{err});
     };
-    response.writeHeader(std.http.Status.ok) catch |err| {
-        std.debug.print("Error writing to response: {any}\n", .{err});
-    };
+    // response.addHeader("content-type", "text/plain") catch |err| {
+    //     std.debug.print("Error writing to response: {any}\n", .{err});
+    // };
     _ = response.write("Hello, world!") catch |err| {
         std.debug.print("Error writing to response: {any}\n", .{err});
     };
@@ -37,8 +39,7 @@ pub fn testPost(req: HttpRequest, response: *HttpResponse) void {
         std.debug.print("{s}: {s}\n", .{ item.key_ptr.*, item.value_ptr.* });
     }
     // const writer = response.writer();
-    const body = req.reader.readAllAlloc(allocator, 1) catch {
-        // const errMsg = "Error reading request body";
+    const body = req.reader.readAllAlloc(allocator, 1) catch { // const errMsg = "Error reading request body";
         // errorResponse(response, std.http.Status.internal_server_error, errMsg);
         response.writeHeader(std.http.Status.internal_server_error) catch {};
         return;
@@ -63,11 +64,11 @@ pub fn testPost(req: HttpRequest, response: *HttpResponse) void {
 
 pub fn main() !void {
     var allocator = std.heap.wasm_allocator;
-    var router = Router.new(allocator);
+    var router = Router.new(allocator).withDebug(true);
     defer router.deinit();
 
     try router.addRoute("/api/testing", testHandler);
     try router.addRoute("/hello", testPost);
-    try router.createStaticRoutes(std.fs.cwd());
+    // try router.createStaticRoutes(std.fs.cwd());
     try router.routeRequest(&allocator);
 }
